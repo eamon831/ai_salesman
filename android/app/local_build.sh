e571f8db88fed5510f1b23b0e639984c60025a36
MD SAIFUL <mdsaiful.rbs@gmail.com>
2025-11-21 11:11:10 +0600

    feat: Add local build script for Android
    This commit introduces a new shell script, `local_build.sh`, to automate the process of building a release APK for the Android application.
    
    The script streamlines development by chaining together several essential commands, ensuring a clean and consistent build every time.
    
    Key steps in the script include:
    - Setting the package name and app name using `change_app_package_name` and `rename_app`.
    - Generating launcher icons with `flutter_launcher_icons`.
    - Cleaning the project and fetching dependencies.
    - Building the release APK.
    - Copying the generated `app-release.apk` to the user's Desktop as `salesman.apk`, replacing any existing file with the same name.

---
+++ android/app/local_build.sh
@@ -0,0 +1,15 @@
+dart run change_app_package_name:main "com.saiful.salesman"
+dart run rename_app:main all="Salesman"
+dart run flutter_launcher_icons:main
+
+flutter clean
+flutter pub get
+flutter build apk --release
+
+# copy the apk to ~/Desktop
+# delete old apk if exists
+if [ -f ~/Desktop/tbhsd.apk ]; then
+  rm ~/Desktop/salesman.apk
+fi
+
+cp build/app/outputs/flutter-apk/app-release.apk ~/Desktop/salesman.apk
